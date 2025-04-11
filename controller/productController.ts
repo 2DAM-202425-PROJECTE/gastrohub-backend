704b8bd5c131b4d348488e0c3c88d05da7b77b94
Christian Villanueva <christianvillanueva@iesebre.com>
2025-04-11 23:29:41 +0200

    update product

--- controller/productController.ts
+++ controller/productController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import Product from "../models/product";
+import ProductIngredient from "../models/productIngredient";
 
 export const getProducts = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -20,7 +21,10 @@ export const getProducts = async (req: Request, res: Response) => {
   }
 };
 
-export const getOneProduct = async (req: Request, res: Response): Promise<any> => {
+export const getOneProduct = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   const { id } = req.params;
 
   try {
@@ -38,7 +42,7 @@ export const getOneProduct = async (req: Request, res: Response): Promise<any> =
       msg: "Talk to the administrator",
     });
   }
-}
+};
 
 export const createProduct = async (req: Request, res: Response) => {
   const { body } = req;
@@ -54,21 +58,51 @@ export const createProduct = async (req: Request, res: Response) => {
   }
 };
 
-export const updateProduct = async (req: Request, res: Response): Promise<any> => {
+export const updateProduct = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   const { id } = req.params;
-  const { body } = req;
+  const { product, ingredients } = req.body;
 
   try {
-    const product = await Product.findByPk(id);
-    if (!product) {
+    const existingProduct = await Product.findByPk(id);
+    if (!existingProduct) {
       return res.status(404).json({
         msg: `There is no product with the id ${id}`,
       });
-    } else {
-      await product.update(body);
-
-      res.json(product);
     }
+
+    await existingProduct.update(product);
+
+    if (ingredients) {
+      for (const [id_ingredient, quantity] of Object.entries(ingredients)) {
+        const existingProductIngredient = await ProductIngredient.findOne({
+          where: {
+            id_product: id,
+            id_ingredient: id_ingredient,
+          },
+        });
+
+        if (existingProductIngredient) {
+          if (quantity === 0) {
+            await existingProductIngredient.destroy();
+          } else {
+            await existingProductIngredient.update({
+              quantity: quantity,
+            });
+          }
+        } else {
+          await ProductIngredient.create({
+            id_product: id,
+            id_ingredient: id_ingredient,
+            quantity: quantity,
+          });
+        }
+      }
+    }
+
+    res.json(existingProduct);
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -77,7 +111,10 @@ export const updateProduct = async (req: Request, res: Response): Promise<any> =
   }
 };
 
-export const deleteProduct = async (req: Request, res: Response): Promise<any> => {
+export const deleteProduct = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   const { id } = req.params;
 
   try {
@@ -86,13 +123,27 @@ export const deleteProduct = async (req: Request, res: Response): Promise<any> =
       return res.status(404).json({
         msg: `There is no product with the id ${id}`,
       });
-    } else {
-      await product.destroy();
-
-      res.json(product);
     }
-  } catch (error) {
-    console.log(error);
+
+    await ProductIngredient.destroy({
+      where: { id_product: id },
+    });
+
+    await product.destroy();
+
+    res.json(product);
+  } catch (error: any) {
+    console.error(error);
+
+    if (
+      error.name === "SequelizeForeignKeyConstraintError" &&
+      error.original?.constraint === "OrderProducts_id_product_fkey"
+    ) {
+      return res.status(400).json({
+        msg: "No se puede eliminar el producto porque hay pedidos que lo incluyen.",
+      });
+    }
+
     res.status(500).json({
       msg: "Talk to the administrator",
     });
