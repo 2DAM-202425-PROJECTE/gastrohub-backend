10954ba8bb990a23df64259c25a059591b948e22
chhriistiaann <103261306+chhriistiaann@users.noreply.github.com>
2025-05-17 17:21:46 +0200

    Update server.ts

--- models/server.ts
+++ models/server.ts
@@ -59,7 +59,7 @@ class Server {
     try {
       await db.authenticate();
       console.log("Database online");
-      await db.sync({ force: true });
+      await db.sync({ force: false });
       console.log("Base de datos conectada y tablas sincronizadas");
       // await setDefaultData();
       console.log("Datos por defecto cargados");
